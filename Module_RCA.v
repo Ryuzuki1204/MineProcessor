34bb07962e23d7799ac20dff396fb45003ca89de
Tejus Kusur <45903569+EvilDoof@users.noreply.github.com>
2020-08-09 17:11:10 +0530

    Changes to RCA code
    Output changed from 5 bit to 4 bit

--- Module_RCA.v
+++ Module_RCA.v
@@ -21,14 +21,14 @@
 module Module_RCA(
     input [3:0] A,
     input [3:0] B,
-    output [4:0] Res
+    output [3:0] Res
     );
 
-wire [2:0] C;
+wire [3:0] C;
 
 Module_HA g1(A[0],B[0],Res[0],C[0]);
 Module_FA g2(A[1],B[1],C[0],Res[1],C[1]);
 Module_FA g3(A[2],B[2],C[1],Res[2],C[2]);
-Module_FA g4(A[3],B[3],C[2],Res[3],Res[4]);
+Module_FA g4(A[3],B[3],C[2],Res[3],C[3]);
 
 endmodule
