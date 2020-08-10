8c6a73826c94aa281aac105759bdb5f977c82d18
Abijith Y.L <51270206+AbijithYL@users.noreply.github.com>
2020-08-10 19:45:37 +0530

    Update Module_RCA.v
    Removed the half adder module.
    Changed the names of the full adder calls.
    **IMP** :  The last carry is now an output of the ripple carry adder.

--- Module_RCA.v
+++ Module_RCA.v
@@ -6,13 +6,16 @@ module Module_RCA(
     input [3:0] A,              // input 1
     input [3:0] B,              // input 2
     output [3:0] Res            // result or output
+    output C;                   // the last carry generated
     );
 
-    wire [3:0] C;                   // Carry bits
+    wire C0;                   // Carry bits
+    wire C1;
+    wire C2;
 
-Module_HA g1(A[0],B[0],Res[0],C[0]);
-Module_FA g2(A[1],B[1],C[0],Res[1],C[1]);
-Module_FA g3(A[2],B[2],C[1],Res[2],C[2]);
-Module_FA g4(A[3],B[3],C[2],Res[3],C[3]);
+    Module_FA fa0(A[0], B[0], 1'b0, Res[0], C0);
+    Module_FA fa1(A[1], B[1], C0, Res[1], C1);
+    Module_FA fa3(A[2], B[2], C1, Res[2], C2);
+    Module_FA fa4(A[3], B[3], C2, Res[3], C);
 
 endmodule
